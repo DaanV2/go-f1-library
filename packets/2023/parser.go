72bbbb6884059718373d3b6d4c91b75e4e00b606
DaanV2 <daanverstraten@hotmail.com>
2024-05-08 21:50:26 +0200

    Adding comments

--- packets/2023/parser.go
+++ packets/2023/parser.go
@@ -1,6 +1,11 @@
 package f1_2023
 
-import "github.com/DaanV2/go-f1-library/encoding"
+import (
+	"errors"
+
+	"github.com/DaanV2/go-f1-library/encoding"
+	"github.com/DaanV2/go-f1-library/enums"
+)
 
 type Parser struct {
 }
@@ -9,84 +14,117 @@ func NewParser() *Parser {
 	return &Parser{}
 }
 
-func (p *Parser) PacketCarDamageData(decoder *encoding.Decoder) (PacketCarDamageData, error) {
-	return ParsePacketCarDamageData(decoder)
-}
-func (p *Parser) PacketCarDamageDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketCarDamageData, error) {
+// PacketCarDamageData parses the packet and returns the data as a PacketCarDamageData struct
+func (p *Parser) PacketCarDamageData(decoder *encoding.Decoder, header PacketHeader) (PacketCarDamageData, error) {
 	return ParsePacketCarDamageDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketCarSetupData(decoder *encoding.Decoder) (PacketCarSetupData, error) {
-	return ParsePacketCarSetupData(decoder)
-}
-func (p *Parser) PacketCarSetupDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketCarSetupData, error) {
+
+// PacketCarSetupData parses the packet and returns the data as a PacketCarSetupData struct
+func (p *Parser) PacketCarSetupData(decoder *encoding.Decoder, header PacketHeader) (PacketCarSetupData, error) {
 	return ParsePacketCarSetupDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketCarStatusData(decoder *encoding.Decoder) (PacketCarStatusData, error) {
-	return ParsePacketCarStatusData(decoder)
-}
-func (p *Parser) PacketCarStatusDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketCarStatusData, error) {
+
+// PacketCarStatusData parses the packet and returns the data as a PacketCarStatusData struct
+func (p *Parser) PacketCarStatusData(decoder *encoding.Decoder, header PacketHeader) (PacketCarStatusData, error) {
 	return ParsePacketCarStatusDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketCarTelemetryData(decoder *encoding.Decoder) (PacketCarTelemetryData, error) {
-	return ParsePacketCarTelemetryData(decoder)
-}
-func (p *Parser) PacketCarTelemetryDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketCarTelemetryData, error) {
+
+// PacketCarTelemetryData parses the packet and returns the data as a PacketCarTelemetryData struct
+func (p *Parser) PacketCarTelemetryData(decoder *encoding.Decoder, header PacketHeader) (PacketCarTelemetryData, error) {
 	return ParsePacketCarTelemetryDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketEventData(decoder *encoding.Decoder) (PacketEventData, error) {
-	return ParsePacketEventData(decoder)
-}
-func (p *Parser) PacketEventDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketEventData, error) {
+
+// PacketEventData parses the packet and returns the data as a PacketEventData struct
+func (p *Parser) PacketEventData(decoder *encoding.Decoder, header PacketHeader) (PacketEventData, error) {
 	return ParsePacketEventDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketFinalClassificationData(decoder *encoding.Decoder) (PacketFinalClassificationData, error) {
-	return ParsePacketFinalClassificationData(decoder)
-}
-func (p *Parser) PacketFinalClassificationDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketFinalClassificationData, error) {
+
+// PacketFinalClassificationData parses the packet and returns the data as a PacketFinalClassificationData struct
+func (p *Parser) PacketFinalClassificationData(decoder *encoding.Decoder, header PacketHeader) (PacketFinalClassificationData, error) {
 	return ParsePacketFinalClassificationDataWithHeader(decoder, header)
 }
+
+// PacketHeader parses the packet and returns the data as a PacketHeader struct
 func (p *Parser) PacketHeader(decoder *encoding.Decoder) (PacketHeader, error) {
 	return ParsePacketHeader(decoder)
 }
-func (p *Parser) PacketLapData(decoder *encoding.Decoder) (PacketLapData, error) {
-	return ParsePacketLapData(decoder)
-}
-func (p *Parser) PacketLapDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketLapData, error) {
+
+// PacketLapData parses the packet and returns the data as a PacketLapData struct
+func (p *Parser) PacketLapData(decoder *encoding.Decoder, header PacketHeader) (PacketLapData, error) {
 	return ParsePacketLapDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketLobbyInfoData(decoder *encoding.Decoder) (PacketLobbyInfoData, error) {
-	return ParsePacketLobbyInfoData(decoder)
-}
-func (p *Parser) PacketLobbyInfoDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketLobbyInfoData, error) {
+
+// PacketLobbyInfoData parses the packet and returns the data as a PacketLobbyInfoData struct
+func (p *Parser) PacketLobbyInfoData(decoder *encoding.Decoder, header PacketHeader) (PacketLobbyInfoData, error) {
 	return ParsePacketLobbyInfoDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketMotionData(decoder *encoding.Decoder) (PacketMotionData, error) {
-	return ParsePacketMotionData(decoder)
-}
-func (p *Parser) PacketMotionDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketMotionData, error) {
+
+// PacketMotionData parses the packet and returns the data as a PacketMotionData struct
+func (p *Parser) PacketMotionData(decoder *encoding.Decoder, header PacketHeader) (PacketMotionData, error) {
 	return ParsePacketMotionDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketParticipantsData(decoder *encoding.Decoder) (PacketParticipantsData, error) {
-	return ParsePacketParticipantsData(decoder)
-}
-func (p *Parser) PacketParticipantsDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketParticipantsData, error) {
+
+// PacketParticipantsData parses the packet and returns the data as a PacketParticipantsData struct
+func (p *Parser) PacketParticipantsData(decoder *encoding.Decoder, header PacketHeader) (PacketParticipantsData, error) {
 	return ParsePacketParticipantsDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketSessionData(decoder *encoding.Decoder) (PacketSessionData, error) {
-	return ParsePacketSessionData(decoder)
-}
-func (p *Parser) PacketSessionDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketSessionData, error) {
+
+// PacketSessionData parses the packet and returns the data as a PacketSessionData struct
+func (p *Parser) PacketSessionData(decoder *encoding.Decoder, header PacketHeader) (PacketSessionData, error) {
 	return ParsePacketSessionDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketSessionHistoryData(decoder *encoding.Decoder) (PacketSessionHistoryData, error) {
-	return ParsePacketSessionHistoryData(decoder)
-}
-func (p *Parser) PacketSessionHistoryDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketSessionHistoryData, error) {
+
+// PacketSessionHistoryData parses the packet and returns the data as a PacketSessionHistoryData struct
+func (p *Parser) PacketSessionHistoryData(decoder *encoding.Decoder, header PacketHeader) (PacketSessionHistoryData, error) {
 	return ParsePacketSessionHistoryDataWithHeader(decoder, header)
 }
-func (p *Parser) PacketTyreSetsData(decoder *encoding.Decoder) (PacketTyreSetsData, error) {
-	return ParsePacketTyreSetsData(decoder)
-}
-func (p *Parser) PacketTyreSetsDataWithHeader(decoder *encoding.Decoder, header PacketHeader) (PacketTyreSetsData, error) {
+
+// PacketSetupData parses the packet and returns the data as a PacketSetupData struct
+func (p *Parser) PacketTyreSetsData(decoder *encoding.Decoder, header PacketHeader) (PacketTyreSetsData, error) {
 	return ParsePacketTyreSetsDataWithHeader(decoder, header)
+}
+
+// PacketMotionExData parses the packet and returns the data as a PacketMotionExData struct
+func (p *Parser) PacketMotionExData(decoder *encoding.Decoder, header PacketHeader) (PacketMotionExData, error) {
+	return ParsePacketMotionExDataWithHeader(decoder, header)
+}
+
+func (p *Parser) ParsePacket(decoder *encoding.Decoder) (any, error) {
+	header, err := p.PacketHeader(decoder)
+	if err != nil {
+		return nil, err
+	}
+
+	switch header.PacketId {
+	case enums.Motion:
+		return p.PacketMotionData(decoder, header)
+	case enums.Session:
+		return p.PacketSessionData(decoder, header)
+	case enums.LapData:
+		return p.PacketLapData(decoder, header)
+	case enums.Event:
+		return p.PacketEventData(decoder, header)
+	case enums.Participants:
+		return p.PacketParticipantsData(decoder, header)
+	case enums.CarSetups:
+		return p.PacketCarSetupData(decoder, header)
+	case enums.CarTelemetry:
+		return p.PacketCarTelemetryData(decoder, header)
+	case enums.CarStatus:
+		return p.PacketCarStatusData(decoder, header)
+	case enums.FinalClassification:
+		return p.PacketFinalClassificationData(decoder, header)
+	case enums.LobbyInfo:
+		return p.PacketLobbyInfoData(decoder, header)
+	case enums.CarDamage:
+		return p.PacketCarDamageData(decoder, header)
+	case enums.SessionHistory:
+		return p.PacketSessionHistoryData(decoder, header)
+	case enums.TyreSets:
+		return p.PacketTyreSetsData(decoder, header)
+	case enums.MotionEx:
+		return p.PacketMotionExData(decoder, header)
+	}
+
+	return nil, errors.New("unknown packet id")
 }
